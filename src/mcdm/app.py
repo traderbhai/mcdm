c79a49132d4103ed118e885c64244cfb6a0c2a70
Maurycy Blaszczak <mgblaszczak@gmail.com>
2023-12-03 10:33:42 +0100

    move into package

--- src/mcdm/app.py
+++ src/mcdm/app.py
@@ -4,13 +4,19 @@ import streamlit as st
 st.title("Multi-Criteria Decision Making with TOPSIS")
 
 st.markdown(
-    "**TOPSIS**, or the Technique for Order of Preference by Similarity to Ideal Solution, is a multi-criteria decision analysis method. This method is used to determine the best solution from a set of alternatives, based on multiple criteria or attributes. The basic idea behind TOPSIS is to identify solutions that are closest to the ideal solution and furthest from the anti-ideal or negative-ideal solution."
+    "**TOPSIS**, or the Technique for Order of Preference by Similarity to Ideal Solution, "
+    "is a multi-criteria decision analysis method. This method is used to determine the best solution "
+    "from a set of alternatives, based on multiple criteria or attributes. "
+    "The basic idea behind TOPSIS is to identify solutions that are closest to the ideal solution "
+    "and furthest from the anti-ideal or negative-ideal solution."
 )
 
 st.write("For more information see:")
 
 st.markdown(
-    'Hwang, C.L.; Lai, Y.J.; Liu, T.Y. (1993). "A new approach for multiple objective decision making". _Computers and Operational Research_. **20** (8): 889-899. [doi](https://en.wikipedia.org/wiki/Doi_(identifier) "Doi (identifier)"):[10.1016/0305-0548(93)90109-v](https://doi.org/10.1016%2F0305-0548%2893%2990109-v).'
+    'Hwang, C.L.; Lai, Y.J.; Liu, T.Y. (1993). "A new approach for multiple objective decision making". '
+    "_Computers and Operational Research_. **20** (8): 889-899. [doi](https://en.wikipedia.org/wiki/Doi_(identifier) "
+    '"Doi (identifier)"):[10.1016/0305-0548(93)90109-v](https://doi.org/10.1016%2F0305-0548%2893%2990109-v).'
 )
 
 st.header("Options")
@@ -25,16 +31,12 @@ st.header("Critera and Score")
 
 options_list = edited_options["Option"].tolist()
 
-criteria_scores = pd.DataFrame(
-    columns=["Criterion", "Weight", "Is Negative", *options_list]
-)
+criteria_scores = pd.DataFrame(columns=["Criterion", "Weight", "Is Negative", *options_list])
 criteria_scores["Criterion"] = criteria_scores["Criterion"].astype(str)
 criteria_scores["Weight"] = criteria_scores["Weight"].astype(float)
 criteria_scores["Is Negative"] = criteria_scores["Is Negative"].astype(bool)
 
-st.write(
-    "2. Add criteria, weights, and specify if the criterion is negative. Provide scores for each option."
-)
+st.write("2. Add criteria, weights, and specify if the criterion is negative. Provide scores for each option.")
 
 edited_criteria_scores = st.data_editor(criteria_scores, num_rows="dynamic")
 
@@ -56,24 +58,16 @@ if not edited_criteria_scores.empty:
         .reset_index()
         .rename(columns={"SqueredScore": "NormalizedScore"})
     )
-    normalization_factor["NormalizedScore"] = (
-        normalization_factor["NormalizedScore"] ** 0.5
-    )
+    normalization_factor["NormalizedScore"] = normalization_factor["NormalizedScore"] ** 0.5
 
-    normalized_weighted = melted_df.merge(
-        normalization_factor, on="Criterion", how="left"
-    )
-    normalized_weighted["NormalizedScore"] = (
-        normalized_weighted["Score"] / normalized_weighted["NormalizedScore"]
-    )
+    normalized_weighted = melted_df.merge(normalization_factor, on="Criterion", how="left")
+    normalized_weighted["NormalizedScore"] = normalized_weighted["Score"] / normalized_weighted["NormalizedScore"]
     normalized_weighted["NormalizedWeightedScore"] = (
         normalized_weighted["NormalizedScore"] * normalized_weighted["Weight"]
     )
-    normalized_weighted["Is Negative"] = normalized_weighted["Is Negative"].fillna(
-        False
-    )
+    normalized_weighted["Is Negative"] = normalized_weighted["Is Negative"].fillna(False)
 
-    positive_criteria = normalized_weighted[normalized_weighted["Is Negative"] == False]
+    positive_criteria = normalized_weighted[~normalized_weighted["Is Negative"]]
     best_positive = (
         positive_criteria.groupby("Criterion")["NormalizedWeightedScore"]
         .max()
@@ -87,7 +81,7 @@ if not edited_criteria_scores.empty:
         .rename(columns={"NormalizedWeightedScore": "IdealWorst"})
     )
 
-    negative_criteria = normalized_weighted[normalized_weighted["Is Negative"] == True]
+    negative_criteria = normalized_weighted[normalized_weighted["Is Negative"]]
     best_negative = (
         negative_criteria.groupby("Criterion")["NormalizedWeightedScore"]
         .min()
@@ -101,12 +95,8 @@ if not edited_criteria_scores.empty:
         .rename(columns={"NormalizedWeightedScore": "IdealWorst"})
     )
 
-    best_worst_positive = best_positive.merge(
-        worst_positive, on="Criterion", how="left"
-    )
-    best_worst_negative = best_negative.merge(
-        worst_negative, on="Criterion", how="left"
-    )
+    best_worst_positive = best_positive.merge(worst_positive, on="Criterion", how="left")
+    best_worst_negative = best_negative.merge(worst_negative, on="Criterion", how="left")
     ideal_best_worst = pd.concat([best_worst_positive, best_worst_negative])
 
     normalized_weighted = normalized_weighted.merge(
@@ -116,27 +106,19 @@ if not edited_criteria_scores.empty:
     )
 
     normalized_weighted["EuclidianDistanceBestPartial"] = (
-        normalized_weighted["NormalizedWeightedScore"]
-        - normalized_weighted["IdealBest"]
+        normalized_weighted["NormalizedWeightedScore"] - normalized_weighted["IdealBest"]
     ) ** 2
     normalized_weighted["EuclidianDistanceWorstPartial"] = (
-        normalized_weighted["NormalizedWeightedScore"]
-        - normalized_weighted["IdealWorst"]
+        normalized_weighted["NormalizedWeightedScore"] - normalized_weighted["IdealWorst"]
     ) ** 2
 
     euclidian = (
-        normalized_weighted.groupby("Option")[
-            ["EuclidianDistanceBestPartial", "EuclidianDistanceWorstPartial"]
-        ]
+        normalized_weighted.groupby("Option")[["EuclidianDistanceBestPartial", "EuclidianDistanceWorstPartial"]]
         .sum()
         .reset_index()
     )
-    euclidian["EuclidianDistanceBest"] = (
-        euclidian["EuclidianDistanceBestPartial"] ** 0.5
-    )
-    euclidian["EuclidianDistanceWorst"] = (
-        euclidian["EuclidianDistanceWorstPartial"] ** 0.5
-    )
+    euclidian["EuclidianDistanceBest"] = euclidian["EuclidianDistanceBestPartial"] ** 0.5
+    euclidian["EuclidianDistanceWorst"] = euclidian["EuclidianDistanceWorstPartial"] ** 0.5
     euclidian["Performance Score"] = euclidian["EuclidianDistanceWorst"] / (
         euclidian["EuclidianDistanceBest"] + euclidian["EuclidianDistanceWorst"]
     )
@@ -145,6 +127,4 @@ if not edited_criteria_scores.empty:
 
     st.dataframe(euclidian[["Option", "Performance Score", "Rank"]], hide_index=True)
 
-st.markdown(
-    "Made with ❤️ by Maurycy Blaszczak ([maurycyblaszczak.com](https://maurycyblaszczak.com/))"
-)
+st.markdown("Made with ❤️ by Maurycy Blaszczak ([maurycyblaszczak.com](https://maurycyblaszczak.com/))")
